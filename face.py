a772b32a9dfee5129cb455e1e664126a0b85182a
neutron <codevjs@gmail.com>
2020-10-24 01:18:22 +0800

    First commit

---
+++ face.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy
+import pickle
+
+face_casecade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+recognizer    = cv2.face.LBPHFaceRecognizer_create()
+cap           = cv2.VideoCapture(0)
+
+recognizer.read("trainner.yml")
+
+labels = { "person_name" : 1 }
+with open("labels.pickle", "rb") as f:
+    og_labels = pickle.load(f)
+    labels    = {v:k for k,v in og_labels.items()}
+
+while(True) :
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_casecade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+
+    for (x,y,w,h) in faces:
+        roi_grey  = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+
+       #recognize ? deep learned
+        id_, conf = recognizer.predict(roi_grey)
+        if conf >= 45: # and conf <= 85:
+            font   = cv2.FONT_HERSHEY_SIMPLEX
+            name   = labels[id_]
+            color  = (255, 255, 255)
+            stroke = 2
+            cv2.putText(frame, name, (x, y), font, 1, color, stroke, cv2.LINE_AA)
+            print(id_)
+            print(name)
+
+        img_item = labels[id_] + ".png"
+        cv2.imwrite(img_item, roi_color)
+
+        color      = (255, 0, 0) #BGR 0-255
+        stroke     = 2
+        end_core_x = x + w
+        end_core_y = y + h
+        cv2.rectangle(frame, (x, y), (end_core_x, end_core_y) ,color, stroke)
+
+    # Display the resulting frame
+    cv2.imshow("CCTV", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
